491d814e6ef46d99bc4e67dd2d8e323853a44eeb
Sanj Singh <sanjaysinghmuchhal@gmail.com>
2022-08-01 14:15:48 +0530

    finalizing changes

--- bot.js
+++ bot.js
@@ -1,22 +1,38 @@
 const ethers = require('ethers')
 const TelegramBot = require("node-telegram-bot-api");
-require('dotenv').config()
-const network = process.env.BLOCKCHAIN_URL; // using rinkeby testnet
-const network_wss = process.env.BLOCKCHAIN_WSS;
-const address = process.env.WALLET_ADDRESS || "";
-const leastBalance = process.env.LEAST_AMOUNT;
-const token = process.env.TELEGRAM_BOT_TOKEN;
-const bot = new TelegramBot(token, { polling: true });
-let chat_id = ""
+const schedule = require('node-schedule');
 
+require('dotenv').config()
+
+// const network = process.env.BLOCKCHAIN_URL; //reading from env (using rinkeby testnet)
+const network_wss = process.env.BLOCKCHAIN_WSS; //reading from env (using rinkeby testnet wss)
+const address = process.env.WALLET_ADDRESS || ""; //reading wallet from env for which we want to listen for Tx)
+const leastBalance = process.env.LEAST_AMOUNT;  // amount/balance at which we should send notification to wallet address owner
+const token = process.env.TELEGRAM_BOT_TOKEN;   // Telegram bot token_id which sends balance updates
+const bot = new TelegramBot(token, { polling: true }); //new bot instanse
+const rule = new schedule.RecurrenceRule(); //scheduler for recurring balance updates on specified time
+const customWsProvider = new ethers.providers.WebSocketProvider(network_wss);
+rule.hour = parseInt(process.env.DAILY_UPDATES_IN_HOURS);
+rule.minute =new schedule.Range(0, 59, parseInt(process.env.DAILY_UPDATES_IN_MINUTES));
+rule.tz = process.env.DAILY_UPDATES_IN_TIMEZONE;
+rule.dayOfWeek = [0, new schedule.Range(0, 6)];
+let chat_id = "";
 
 var init = function() {
-
+    console.log("started wss connection!")
+    
+    //scheduler for receiving balance updates
+    schedule.scheduleJob(rule, function() {
+        fetchBalance(address);
+    });
+    
+    //telegram bot chat_id intializer
     bot.on("message", async (msg) => {
 
         const { chat: { id }, text } = msg;
+
         chat_id = id;
-        // console.log("printing tel: ",msg, id, text)
+        
         try {
             bot.sendMessage(chat_id, ` You'll recieve balance updates here`);
         } catch (error) {
@@ -25,28 +41,13 @@ var init = function() {
     
     });
 
-    const provider = ethers.getDefaultProvider(network);
-    const customWsProvider = new ethers.providers.WebSocketProvider(network_wss);
-    console.log("started wss connection!")
-
+    //incoming transaction handler
     customWsProvider.on("pending", (tx) => {
         customWsProvider.getTransaction(tx).then(function (transaction) {
             let filter = transaction ? transaction.from === address || transaction.to === address : false;
             if(filter){
                 console.log(transaction);
-                customWsProvider.getBalance(address).then((balance) => {
-                    // convert a currency unit from wei to ether
-                    const balanceInEth = ethers.utils.formatEther(balance)
-                    console.log(`balance: ${balanceInEth} ETH`)
-                    if(balanceInEth < leastBalance){
-                        //send notification to telegram
-                        try {
-                            bot.sendMessage(chat_id, ` your currecnt balance is ${balanceInEth}, please add more to avoid anykind of issue`);
-                        } catch (error) {
-                            bot.sendMessage(chat_id, `something went wrong while sending message`);
-                        }
-                    }
-                })
+                fetchBalance(address);
             }
         })
     })
@@ -62,6 +63,25 @@ var init = function() {
         setTimeout(init, 3000);
     });
 
+}
+
+function fetchBalance(address){
+    customWsProvider.getBalance(address).then((balance) => {
+        // convert a currency unit from wei to ether
+        const balanceInEth = ethers.utils.formatEther(balance)
+        console.log(`balance: ${balanceInEth} ETH`)
+        if(balanceInEth < leastBalance && chat_id){
+            //send notification to telegram
+            try {
+                bot.sendMessage(chat_id, ` your currecnt balance is ${balanceInEth}, please add more to avoid anykind of issue`);
+            } catch (error) {
+                bot.sendMessage(chat_id, `something went wrong while sending message`);
+            }
+        }
+        if(!chat_id){
+            console.log("please type /start to telegram bot to start recieving balance updates")
+        }
+    })
 }
 
 init();
